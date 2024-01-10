493d68e42941c649b7838654b3ada0a06039ed86
fentosv <fentos.dev@gmail.com>
2024-01-10 22:39:19 +0100

    Update colors methods

--- src/components/ColorPicker.svelte
+++ src/components/ColorPicker.svelte
@@ -1,33 +1,55 @@
 <script>
   import colors from 'utils/colors.ts';
 
+  const colorsToParse = [
+    'red',
+    '#892987',
+    'hsl(60 50% 10% / 100%)',
+    'lab(52.2345% 40.1645 59.9971)',
+    'oklch(70% 0.1 159)',
+  ];
+
+  let colorHex = '#ffffff';
   let color = '#ffffff';
-  let okColor = color;
+  let colorOklch = null;
   let colorArray = [];
-
-  function formatNum(num) {
-    return num.toFixed(4);
-  }
-
-  function stringNum(L, C, H) {
-    return `oklch( ${L} ${C} ${H})`;
-  }
+  let errorColor = null;
 
   function handleChange(e) {
     const ogColor = e.target.value;
-    colors(ogColor);
+    const { oklchColor, hexColor, cssOklchColor } = colors(ogColor);
+    color = ogColor;
+    colorHex = hexColor;
+    colorOklch = cssOklchColor;
   }
 </script>
 
 <article>
-  Count: {color}
-  <input type="color" on:input={handleChange} bind:value={color} name="color" />
+  Color: {color}
+  <input type="color" on:input={handleChange} bind:value={colorHex} name="color" />
 
-  <div style={`background-color: ${color};`}>Original</div>
-  <div style={`background-color: ${okColor};`}>Result</div>
+  <div class="colors" style={`background-color: ${color};`}>Original</div>
+  <div class="colors" style={`background-color: ${colorOklch};`}>Result</div>
+
   {#each colorArray as genColor}
     <div style={`background-color: ${genColor};`}>Result</div>
   {/each}
+
+  <div style="display: flex; flex-direction: column;">
+    {#each colorsToParse as colorToParse}
+      <label>
+        <input type="radio" on:input={handleChange} value={colorToParse} name="listColor" />
+        {colorToParse}
+      </label>
+    {/each}
+  </div>
+
+  {#if colorOklch}
+    <p>Your oklch color: <strong>{colorOklch}</strong></p>
+  {/if}
+  {#if errorColor}
+    <p>Color error: <strong>{errorColor}</strong></p>
+  {/if}
 </article>
 
 <style>
@@ -38,10 +60,9 @@
     align-items: center;
   }
 
-  div {
-    width: 200px;
+  .colors {
+    width: 250px;
   }
-
   /* 
    - L (lightness): 0-1. Black to white
    - C (chroma): 0-0.5.Saturation
--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,12 +1,53 @@
-import { parseHex, converter, useMode, modeOklch } from 'culori/fn'
+import {
+  converter,
+  formatHex,
+  useMode,
+  modeOklch,
+  modeRec2020,
+  modeOklab,
+  modeXyz65,
+  modeRgb,
+  modeLch,
+  modeHsl,
+  modeLab,
+  modeP3,
+  parse,
+  type Oklch,
+  type Color,
+} from 'culori/fn'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const oklch = useMode(modeOklch)
+export const oklch = useMode(modeOklch)
+export const rec2020 = useMode(modeRec2020)
+export const oklab = useMode(modeOklab)
+export const xyz65 = useMode(modeXyz65)
+export const rgb = useMode(modeRgb)
+export const lch = useMode(modeLch)
+export const hsl = useMode(modeHsl)
+export const lab = useMode(modeLab)
+export const p3 = useMode(modeP3)
+
 const oklchConverter = converter('oklch')
 
-export default function colors(color: string) {
-  // console.log(color)
-  // console.log(oklch(color))
+function formatOklchValue(num: number) {
+  //Delete trailing 0 and only 3 decimals
+  return parseFloat(num.toFixed(3))
+}
 
-  console.log(oklchConverter(parseHex(color)))
+function formatOklch(color: Oklch) {
+  const { l, c, h } = color
+  return `oklch(${formatOklchValue(l)} ${formatOklchValue(c)} ${h ? formatOklchValue(h) : ''})`
+}
+
+export default function colors(color: string) {
+  const parsedColor = parse(color)
+  const hexColor = formatHex(color)
+  const oklchColor = oklchConverter(color)
+  const cssOklchColor = formatOklch(oklchColor)
+
+  return {
+    hexColor,
+    parsedColor,
+    oklchColor,
+    cssOklchColor,
+  }
 }
