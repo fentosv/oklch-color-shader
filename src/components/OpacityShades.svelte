fdc79fa1c7dcd46520a1ef6495207a0ad89af1de
fentosv <fentos.dev@gmail.com>
2024-01-11 20:56:05 +0100

    Add OpacityShades base component

---
+++ src/components/OpacityShades.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { type Oklch } from 'culori/fn';
+  import { getOpacityShades } from 'utils/colors';
+
+  export let color: Oklch;
+  let colorArray: string[] = [];
+
+  getOpacityShades(color);
+</script>
+
+<article>
+  Color: {color}
+
+  {#if colorArray.length > 0}
+    {#each colorArray as genColor}
+      <div style={`background-color: ${genColor};`}>Result</div>
+    {/each}
+  {/if}
+</article>
+
+<style>
+  article {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
