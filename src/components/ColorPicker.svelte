136c305b05a74c41c6605e37fcdf769902bd05a2
fentosv <fentos.dev@gmail.com>
2024-01-12 18:02:12 +0100

    Move dev color selector to /dev/ColorSelector

--- src/components/ColorPicker.svelte
+++ src/components/ColorPicker.svelte
@@ -4,16 +4,9 @@
 
   import { type Oklch } from 'culori/fn';
   import OpacityShades from './OpacityShades.svelte';
+  import ColorSelector from './dev/ColorSelector.svelte';
 
   import { formatOklchToString, convertToOklch, formatToHex } from 'utils/colors.ts';
-
-  const colorsToParse = [
-    'red',
-    '#892987',
-    'hsl(60 50% 10% / 100%)',
-    'lab(52.2345% 40.1645 59.9971)',
-    'oklch(70% 0.1 159)',
-  ];
 
   let color = '#ff7000';
   let colorHex = color;
@@ -36,6 +29,7 @@
 
   function handleChange(e: Event) {
     const target = e.target as HTMLInputElement;
+    console.log(target);
     const ogColor = target.value;
     color = ogColor;
   }
@@ -44,20 +38,13 @@
 <article>
   Color: {color}
 
+  <ColorSelector bind:color={color}></ColorSelector>
+
   <input type="color" on:input={handleChange} bind:value={colorHex} name="color" />
   <input type="text" on:input={handleChange} bind:value={color} name="color" />
 
   <div class="colors" style={`background-color: ${color};`}>Original</div>
   <div class="colors" style={`background-color: ${colorOklchCss};`}>In Oklch</div>
-
-  <div style="display: flex; flex-direction: column;">
-    {#each colorsToParse as colorToParse}
-      <label>
-        <input type="radio" on:input={handleChange} value={colorToParse} name="listColor" />
-        {colorToParse}
-      </label>
-    {/each}
-  </div>
 
   {#if colorOklch}
     <p>Your oklch color: <strong>{colorOklchCss}</strong></p>
---
+++ src/components/dev/ColorSelector.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  const colorsToParse = [
+    'red',
+    '#892987',
+    'hsl(60 50% 10% / 100%)',
+    'lab(52.2345% 40.1645 59.9971)',
+    'oklch(70% 0.1 159)',
+  ];
+
+  export let color;
+
+  function handleChange(e: Event) {
+    const target = e.target as HTMLInputElement;
+    color = target.value;
+  }
+</script>
+
+<article>
+  {#each colorsToParse as colorToParse}
+    <label>
+      <input type="radio" on:input={handleChange} value={colorToParse} name="listColor" />
+      {colorToParse}
+    </label>
+  {/each}
+</article>
+
+<style>
+  article {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
