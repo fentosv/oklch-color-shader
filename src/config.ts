ebe6d36ba3ebe1ad256ae5328dbb224a2955b12d
fentosv <fentos.dev@gmail.com>
2024-01-12 20:14:49 +0100

    Update page title

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import type { Site, SocialObjects } from './types'
 
 export const SITE: Site = {
   description: 'Your meta description.',
-  name: 'Fentos Template',
+  name: 'OKLCK Color Shader',
   website: 'https://fentos.template',
 }
 
