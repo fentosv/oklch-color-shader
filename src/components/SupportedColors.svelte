3d7a5947db4ced1ffdb5f3ddfa319b965833f53a
fentosv <fentos.dev@gmail.com>
2024-01-13 13:22:09 +0100

    Add conditional classes in SupportedColors

--- src/components/SupportedColors.svelte
+++ src/components/SupportedColors.svelte
@@ -3,13 +3,25 @@
 </script>
 
 <section>
-  <p>oklch {$support.oklch ? 'is supported in this device' : 'is not supported in this device'}</p>
-  <p>p3 {$support.p3 ? 'is supported in this device' : 'is not supported in this device'}</p>
+  <p class:on={$support.oklch}>
+    oklch {$support.oklch ? 'is supported in this device' : 'is not supported in this device'}
+  </p>
+  <p class:on={$support.p3}>
+    p3 {$support.p3 ? 'is supported in this device' : 'is not supported in this device'}
+  </p>
 </section>
 
 <style>
   section {
     display: flex;
     flex-direction: column;
+  }
+
+  p {
+    color: red;
+  }
+
+  .on {
+    color: green;
   }
 </style>
