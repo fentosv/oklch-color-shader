4fde126bfa3c94d592d06916b2c36348b2c74e91
fentosv <fentos.dev@gmail.com>
2024-01-14 20:23:26 +0100

    Add ColorCard to ShadesLayout

--- src/components/ShadesLayout.svelte
+++ src/components/ShadesLayout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { type Oklch } from 'culori/fn';
+  import ColorCard from './ColorCard.svelte';
   import OpacityShades from './OpacityShades.svelte';
 
   export let color: Oklch;
@@ -7,6 +8,8 @@
 
 <article>
   <h2>Shades layout component</h2>
+  <p>Original color in Oklch</p>
+  <ColorCard color={color} />
   <OpacityShades color={color} />
 </article>
 
